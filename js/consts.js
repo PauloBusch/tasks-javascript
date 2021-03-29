3b180d454a30027a3919c8159ce5d9788eb84416
Paulo Ricardo Busch <paulo202015@outlook.com.br>
2021-03-29 19:38:30 -0300

    Login submit

---
+++ js/consts.js
@@ -0,0 +1,2 @@
+const OAPI_HOST = 'http://localhost:3003/oapi';
+const API_HOST = 'http://localhost:3003/api';
--- js/pages/login.js
+++ js/pages/login.js
@@ -10,9 +10,40 @@ function loginSubmit(event) {
   event.stopPropagation();
   const loginForm = document.getElementById('login-form');
   loginForm.classList.add('was-validated');
-  if (loginForm.checkValidity()) postLogin(loginForm);
+  if (loginForm.checkValidity()) postLogin();
 }
 
-function postLogin(loginForm) {
-  const formData = new FormData(loginForm);
+function postLogin() {
+  const button = document.getElementById('submit-login');
+  const request = new XMLHttpRequest();
+  button.classList.add('disabled');
+  request.open('POST', `${OAPI_HOST}/login`, true);
+  request.onload = function(result) {
+    button.classList.remove('disabled');
+    const response = JSON.parse(result.currentTarget.response);
+    if (result.currentTarget.status === 200) {
+      localStorage.setItem('token', response.data.token);
+      location.href = 'index.html';
+      return;
+    }
+
+    if (response.errors && response.errors.length > 0) {
+      const credentialsError = 'Invalid email or password';
+      if (response.errors.some(e => e === credentialsError)) {
+        alert('Usuário/Senha inválidos!');
+        return;
+      }
+    }
+
+    alert('Erro ao realizar login!');
+  };
+  request.setRequestHeader('Content-Type', 'application/json');
+  request.send(JSON.stringify(getLoginData()));
+}
+
+function getLoginData() {
+  return {
+    email: document.getElementById('email').value,
+    password: document.getElementById('password').value
+  };
 }
