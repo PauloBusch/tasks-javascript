0ce95b36dc2eee8c42a5656e6eb3429be29d4730
Paulo Ricardo Busch <paulo202015@outlook.com.br>
2021-03-29 20:40:24 -0300

    Active menu based in hash url

--- js/pages/sidenav.js
+++ js/pages/sidenav.js
@@ -4,6 +4,9 @@ function sidenavEvents() {
   const menus = document.querySelectorAll('#menu .nav-link');
   for (const menu of menus)
     menu.addEventListener('click', toggleMenu, false);
+
+  const menuActiveHash = document.querySelector(`a[href="${location.hash || '#dashboard'}"]`);
+  menuActiveHash.classList.add('active');
 }
 
 function toggleMenu(event) {
